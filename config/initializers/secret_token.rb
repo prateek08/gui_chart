747280baa08bbe835175bd3fa3fbc0f82c780e39
Prateek Agarwal <prtkagarwal08@gmail.com>
2012-07-06 10:48:06 +0530

    first commit 0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuiChart::Application.config.secret_token = 'bd237d2aa56175d129b8bb4a10ce51a5cf8dbb2a4ea81c63d20dc3cf1e9b6da00f2a127986aaf992ea64adee09f223bb8566aa02139a5a0bfa173ec9e38ecb78'
